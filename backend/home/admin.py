a09688bb5b12ce9d32da70a4d933fba3d6494c61
crowdbotics-dev <engineering@crowdbotics.com>
2023-07-19 17:46:18 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Sderr
+admin.site.register(Sderr)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,12 @@
+from django.conf import settings
 from django.db import models
+class Sderr(models.Model):
+    'Generated Model'
+    bdbdh = models.BigIntegerField()
+    oerrur = models.BigIntegerField()
+    def test_method(self):
+        import requests
+        response = requests.get("https://hello.com")
+        data = response.json()
+        print(data)
+        return data
